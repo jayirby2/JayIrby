81856669b643debf9a60db22c0e458a3538b451a
Dale <dalebitter274@gmail.com>
2023-10-31 15:13:10 -0400

    added decode function

--- Lab6.py
+++ Lab6.py
@@ -1,9 +1,15 @@
-# Jay Irby encoder
-def encode(pass):
+# Jay Irby encoder edited by Dale Bittner
+def encode(password):
     encoded_pass = ''
     for i in password:
         encoded_pass += str((int(i) + 3))
     return encoded_pass
+
+def decode(password):
+    original_pass = ''
+    for i in password:
+        original_pass += str((int(i) - 3))
+    return original_pass
 def main():
     def menu():
         print('Menu')
@@ -19,7 +25,7 @@ def main():
         if user_input == 1:
             print('Please enter your password to encode:', end=' ')
             password = str(input())
-            encoded_pass = encoder(password)
+            encoded_pass = encode(password)
             print('Your password has been encoded and stored!')
             print('')
             menu()
@@ -27,7 +33,7 @@ def main():
 
         elif user_input == 2:
             decode(encoded_pass)
-            print(f'The encoded password is {encoded_pass}, and the original password is {original_pass}')
+            print(f'The encoded password is {encoded_pass}, and the original password is {decode(encoded_pass)}')
             print('')
             menu()
             user_input = int(input())
